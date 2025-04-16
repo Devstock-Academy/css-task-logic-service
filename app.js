bb7218fed62a8b45a05b853437385eb296171909
RafalKielbasa <kielbasarafal92@gmail.com>
2025-04-16 09:57:33 +0200

    chore: result console.log

--- app.js
+++ app.js
@@ -1,87 +1,88 @@
-import express from "express";
-import pixelmatch from 'pixelmatch';
-import sharp from 'sharp';
-import { promisify } from 'util';
-import puppeteer from 'puppeteer-core';
-import dotenv from 'dotenv';
-import {PNG} from 'pngjs';
-import * as cheerio from 'cheerio';
-import chromium from '@sparticuz/chromium';
+import express from 'express'
+import pixelmatch from 'pixelmatch'
+import sharp from 'sharp'
+import { promisify } from 'util'
+import puppeteer from 'puppeteer-core'
+import dotenv from 'dotenv'
+import { PNG } from 'pngjs'
+import * as cheerio from 'cheerio'
+import chromium from '@sparticuz/chromium'
 
-dotenv.config();
-PNG.prototype.promisifyParse = promisify(PNG.prototype.parse);
+dotenv.config()
+PNG.prototype.promisifyParse = promisify(PNG.prototype.parse)
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
 app.post('/', async (req, res) => {
+  const { targetUrl, html } = req.body
 
-  const {targetUrl, html} = req.body
-  
   try {
     const browser = await puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath(),
       headless: chromium.headless,
-    });
-    const page = await browser.newPage();
+    })
+    const page = await browser.newPage()
 
-    await page.setViewport({ width: 333, height: 266 });
+    await page.setViewport({ width: 333, height: 266 })
 
-    const $ = cheerio.load(html);
-    const answerImageHTML = $.html();
+    const $ = cheerio.load(html)
+    const answerImageHTML = $.html()
 
-    await page.setContent(answerImageHTML);
+    await page.setContent(answerImageHTML)
 
-    const answerImagePng = await page.screenshot({ type: 'png' });
+    const answerImagePng = await page.screenshot({ type: 'png' })
 
-    await browser.close();
+    await browser.close()
 
     const targetImageBuffer = Buffer.from(
-      await (await fetch(targetUrl)).arrayBuffer(),
-    );
+      await (await fetch(targetUrl)).arrayBuffer()
+    )
 
-    const trimedTargetImage = await sharp(targetImageBuffer).toBuffer();
+    const trimedTargetImage = await sharp(targetImageBuffer).toBuffer()
     const targetImageToCheck = await new PNG({}).promisifyParse(
-      trimedTargetImage,
-    );
+      trimedTargetImage
+    )
 
-    const { width, height } = targetImageToCheck;
+    const { width, height } = targetImageToCheck
 
     const trimedAnswerImage = await sharp(answerImagePng)
       .resize(width, height)
-      .toBuffer();
+      .toBuffer()
 
     const answerImageToCheck = await new PNG({}).promisifyParse(
-      trimedAnswerImage,
-    );
+      trimedAnswerImage
+    )
 
     const missmatch = pixelmatch(
       answerImageToCheck.data,
       targetImageToCheck.data,
       null,
       width,
-      height,
-    );
+      height
+    )
 
-    const pixlesQty = width * height;
+    const pixlesQty = width * height
 
     const result = Number(
-      (((pixlesQty - missmatch) / pixlesQty) * 100).toFixed(0),
-    );
+      (((pixlesQty - missmatch) / pixlesQty) * 100).toFixed(0)
+    )
+
+    console.log({ result })
+
     // todo: hardcore level, easy level - pixel perfect or not
-    res.send(JSON.stringify({result:String(result > 97 ? 100 : result)}))
+    res.send(JSON.stringify({ result: String(result > 97 ? 100 : result) }))
   } catch (error) {
-    console.error('COUNT MATCH ERROR: ', error);
+    console.error('COUNT MATCH ERROR: ', error)
     res.send(JSON.stringify(0))
   }
-  })
+})
 
-app.listen(PORT , () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
-
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
